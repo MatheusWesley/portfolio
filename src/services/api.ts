f247a0fbbcb59be3213c39dd003b32b62c398645
MatheusWesley <matheus.tarra@gmail.com>
2024-02-25 11:33:04 -0300

    Preparando a o aquivo api.ts para que você configure de acordo com seu github

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import { Octokit } from "@octokit/core"
 
 const octokit = new Octokit({
-  auth: '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'
+  auth: 'CHAVE_API_AQUI'
 })
 
-export const api = await octokit.request('GET /users/MatheusWesley/repos', {
+export const api = await octokit.request('GET /users/USERNAME/repos', {
   username: 'MatheusWesley',
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
